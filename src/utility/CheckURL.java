d58fbe07c8148884fc57221eff93bc82d2c4f2a9
U22aki <akiseidaisuki@gmail.com>
2016-07-14 15:52:22 +0900

    2016/7/14 Commit

---
+++ src/utility/CheckURL.java
@@ -0,0 +1,13 @@
+package utility;
+
+public class CheckURL {
+	private boolean correctURL = false;
+	
+	/**
+	 * URLをチェック
+	 */
+	public boolean checkURL(String url){
+		String regex = ""
+		return correctURL;
+	}
+}
